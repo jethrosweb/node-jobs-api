38d1df82db50b75cfca2c581c65697138f13453c
Jethro Dean <jethroldean@gmail.com>
2022-06-06 09:28:05 +1000

    security added, project complete

--- starter/app.js
+++ starter/app.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const authenticateUser = require('./middleware/authentication')
 
+// security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // connect DB
 const connectDB = require('./db/connect')
 
@@ -15,7 +21,19 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+  })
+)
+
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // extra packages
 
 // use routes
